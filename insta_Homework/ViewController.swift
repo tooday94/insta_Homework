c668fb464345b665e00697bd4768ba307ffe3c88
tooday94 <masharipovbakhrom@gmail.com>
2023-06-23 12:57:31 +0500

    Initial Commit

---
+++ insta_Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  insta_Homework
+//
+//  Created by Bakhrom Masharipov on 23.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
